971f713021157dc1b55fdafa382f4021738bf7a3
IvanGaenko <gaenko37@gmail.com>
2023-07-01 18:45:28 +0300

    Refactor canvas dimensions calculator v6

--- src/components/Canvas.ts
+++ src/components/Canvas.ts
@@ -14,12 +14,18 @@ class Canvas {
     snake.id = "snake";
     apple.id = "apple";
 
-    background.width = this.canvasContainer.clientWidth;
-    background.height = this.canvasContainer.clientHeight;
-    snake.width = this.canvasContainer.clientWidth;
-    snake.height = this.canvasContainer.clientHeight;
-    apple.width = this.canvasContainer.clientWidth;
-    apple.height = this.canvasContainer.clientHeight;
+    // background.width = this.canvasContainer.clientWidth;
+    // background.height = this.canvasContainer.clientHeight;
+    // snake.width = this.canvasContainer.clientWidth;
+    // snake.height = this.canvasContainer.clientHeight;
+    // apple.width = this.canvasContainer.clientWidth;
+    // apple.height = this.canvasContainer.clientHeight;
+    background.width = 300;
+    background.height = 300;
+    snake.width = 300;
+    snake.height = 300;
+    apple.width = 300;
+    apple.height = 300;
 
     this.canvasContainer.appendChild(background);
     this.canvasContainer.appendChild(snake);
